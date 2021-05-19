0a5a521e1bcf46a81bc37fabdb4208dd7b062135
aryanrvmali <aryanmali@users.noreply.github.com>
2021-05-19 08:47:25 +0530

    609

---
+++ lc-609.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<string>> findDuplicate(vector<string>& paths) {
+        unordered_map<string, vector<string>> mp;
+        vector<vector<string>> ans;
+        for(auto path : paths) {
+            stringstream ss(path);
+            string root,s;
+            getline(ss,root,' ');
+            while(getline(ss,s,' ')) {
+                string filepath = root + '/' + s.substr(0, s.find('('));
+                string filecontent = s.substr(s.find('(') + 1, s.find(')') - s.find('(') - 1);
+                mp[filecontent].push_back(filepath);
+            }
+        }
+        for(auto x:mp) {
+            if(x.second.size()>1)
+                ans.push_back(x.second);
+        }
+        return ans;
+    }
+};
